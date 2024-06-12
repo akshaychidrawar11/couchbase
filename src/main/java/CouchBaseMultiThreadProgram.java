d0ad463c5e0cf1e586011a9ea23ade46cbbe9568
akshaychidrawar11 <akshay.chidrawar11@gmail.com>
2024-06-12 15:39:37 +0530

    couchbase trail3

--- src/main/java/CouchBaseMultiThreadProgram.java
+++ src/main/java/CouchBaseMultiThreadProgram.java
@@ -24,12 +24,11 @@ public class CouchBaseMultiThreadProgram {
 
         Cluster cluster = Cluster.connect(COUCHBASE_CONNECTION_STRING, COUCHBASE_USERNAME, COUCHBASE_PASSWORD);
         Bucket bucket = cluster.bucket(BUCKET_NAME);
-        // int[] threadPoolSizes = {2, 4, 8}; // Different thread pool sizes for testing
         final int NUM_THREADS = 2;// Different thread pool sizes for testing
         System.out.println("Testing with thread pool size: " + NUM_THREADS);
         ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
-        long endTime = System.currentTimeMillis(); // Run for 3 minutes
-        // long endTime = System.currentTimeMillis() + 3 * 60 * 1000; // 3 minutes
+        long endTime = System.currentTimeMillis() + 3 * 60 * 1000; // 3 minutes
+        System.out.println("Start time : " + System.currentTimeMillis());
         for (int i = 0; i < NUM_THREADS; i++) {
             executor.submit(() -> performCouchbaseOperations(bucket, endTime));
         }
@@ -42,15 +41,19 @@ public class CouchBaseMultiThreadProgram {
         cluster.disconnect();
     }
 
+    /**
+     * read json data from file location and insert/read from couchbase
+     * @param bucket
+     * @param endTime
+     */
     private static void performCouchbaseOperations(Bucket bucket,Long  endTime) {
-        while (System.currentTimeMillis() < endTime) {
+          while (System.currentTimeMillis() < endTime) {
             try {
-                String key = "aktest3";
+                String key = "aktest3";//key value we can change
                 File jsonFile = new File(FILE_PATH);
                 Collection collection = bucket.scope(REGION_NAME).collection(COLLECTION_NAME);
                 String jsonContent = new String(Files.readAllBytes(jsonFile.toPath()));
                 JsonObject jsonObject = JsonObject.fromJson(jsonContent);
-                // String key = UUID.randomUUID().toString();
                 Long threadId = Thread.currentThread().getId();
                 insertJsonData(key, collection, jsonObject, threadId);
                 for (int i = 0; i < 3; i++) {
@@ -60,19 +63,32 @@ public class CouchBaseMultiThreadProgram {
                 e.printStackTrace();
             }
         }
+        System.out.println("End time : " + System.currentTimeMillis());
     }
 
+    /**
+     * perform read operation from couchbase
+     * @param key
+     * @param collection
+     * @param threadId
+     */
     private static void readJsonData(String key, Collection collection, Long threadId) {
         long startTime;
         startTime = System.nanoTime();
         GetResult result = collection.get(key);
         long retrievalTime = System.nanoTime() - startTime;
         JsonObject retrievedJson = result.contentAsObject();
-        //    System.out.println("!!Retrieved JSON: " + retrievedJson);
         System.out.println("Retrieved document: " + retrievedJson + " in " + retrievalTime + " ns for thread " + threadId);
     }
 
-    private static void insertJsonData(String key, Collection collection, JsonObject jsonObject,Long threadId) {
+    /**
+     * perform write/insert operation
+     * @param key
+     * @param collection
+     * @param jsonObject
+     * @param threadId
+     */
+    private static void insertJsonData(String key, Collection collection, JsonObject jsonObject, Long threadId) {
         long startTime = System.nanoTime();
         collection.upsert(key, jsonObject);
         System.out.println("Inserted document with key: " + key + " in " + (System.nanoTime() - startTime) + " ns for thread " + threadId);
